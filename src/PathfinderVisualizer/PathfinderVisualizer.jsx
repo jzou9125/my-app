074b8c6007b9098875014c7351dff935b843dfb2
jason zou <jasonzou@buffalo.edu>
2023-01-03 19:06:56 -0500

    project rebuild

---
+++ src/PathfinderVisualizer/PathfinderVisualizer.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export const PathfinderVisualizer = (props) => {
+  const grid = useState([]);
+  const width = useState(window.innerWidth);
+  const height = useState(window.innerHeight);
+  const square_size = 35;
+
+  useEffect(() => {
+    
+}, []);
+
+  return <div> Hopefully this time it works </div>;
+};
