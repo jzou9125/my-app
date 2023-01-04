18fd7233723a15d66234ddd0ab6c0302d92950be
jason zou <jasonzou@buffalo.edu>
2023-01-03 19:26:34 -0500

    we store a state that is dynamically sized to window size

--- src/PathfinderVisualizer/PathfinderVisualizer.jsx
+++ src/PathfinderVisualizer/PathfinderVisualizer.jsx
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
 
 export const PathfinderVisualizer = (props) => {
-  const grid = useState([]);
-  const width = useState(window.innerWidth);
-  const height = useState(window.innerHeight);
+  const [grid, setGrid] = useState([]);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
   const square_size = 35;
 
   useEffect(() => {
-    
-}, []);
+    let newGrid = [];
+    const rows = height / square_size;
+    const columns = width / square_size;
+    for (let row = 0; row < rows; row++) {
+      let currentRow = [];
+      for (let column = 0; column < columns; column++) {
+        currentRow.push(0);
+      }
+      newGrid.push(currentRow);
+    }
+    setGrid(newGrid);
+  }, []);
 
-  return <div> Hopefully this time it works </div>;
+  console.log(grid);
+  return <div>welp</div>;
 };
