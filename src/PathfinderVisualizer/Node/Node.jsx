db2778dd78585a025755b958ce2dfd2dcc0b30ee
jason zou <jasonzou@buffalo.edu>
2023-01-13 15:59:49 -0500

    Grid with a start/finish node on the board.

--- src/PathfinderVisualizer/Node/Node.jsx
+++ src/PathfinderVisualizer/Node/Node.jsx
@@ -1,1 +1,14 @@
-    
+import "./Node.css";
+const Node = ({ row, column, isStart, isFinish, isVisited, isWall }) => {
+  const extraClassName = isStart
+    ? "node-start"
+    : isFinish
+    ? "node-finish"
+    : isWall
+    ? "wall"
+    : isVisited
+    ? "visited"
+    : "";
+  return <div className={`node ${extraClassName}`}> </div>;
+};
+export default Node;
--- src/PathfinderVisualizer/PathfinderVisualizer.jsx
+++ src/PathfinderVisualizer/PathfinderVisualizer.jsx
@@ -1,25 +1,68 @@
 import { useEffect, useState } from "react";
+import Node from "./Node/Node";
+import "./PathfinderVisualizer.css";
 
 export const PathfinderVisualizer = (props) => {
   const [grid, setGrid] = useState([]);
   const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
-  const square_size = 35;
+  const [height, setHeight] = useState(window.innerHeight - 75); //75 needs to be replaced with the size of other components
+  const [rows, setRows] = useState(0);
+  const [columns, setColumns] = useState(0);
+  const [start, setStart] = useState([0, 0]);
+  const [end, setEnd] = useState([0, 0]);
+  const square_size = 32;
 
   useEffect(() => {
+    setStart([Math.floor(rows / 3), Math.floor(columns / 3)]);
+    setEnd([Math.floor(rows / 3), Math.floor((columns / 3) * 2)]);
+  }, [rows, columns]);
+
+  useEffect(() => {
+    const newRows = Math.floor(height / square_size);
+    const newColumns = Math.floor(width / square_size);
+    setRows(newRows);
+    setColumns(newColumns);
+    setGrid(initGrid(newRows, newColumns));
+  }, []);
+
+  const initGrid = (rows, columns) => {
     let newGrid = [];
-    const rows = height / square_size;
-    const columns = width / square_size;
     for (let row = 0; row < rows; row++) {
       let currentRow = [];
       for (let column = 0; column < columns; column++) {
-        currentRow.push(0);
+        currentRow.push(nodeProps(row, column));
       }
       newGrid.push(currentRow);
     }
-    setGrid(newGrid);
-  }, []);
+    return newGrid;
+  };
 
-  console.log(grid);
-  return <div>welp</div>;
+  const nodeProps = (row, column) => {
+    return {
+      row,
+      column,
+      distance: Infinity,
+      isVisited: false,
+      isWall: false,
+      previousNode: null,
+    };
+  };
+  return (
+    <div>
+      <div> Header menu</div>
+      <div className="grid">
+        {grid.map((row, x) =>
+          row.map((node, y) => (
+            <Node
+              key={`${x}-${y}`}
+              isStart={node.row === start[0] && node.column === start[1]}
+              isFinish={node.row === end[0] && node.column === end[1]}
+              isVisited={node.isVisited}
+              isWall={node.isWall}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
 };
